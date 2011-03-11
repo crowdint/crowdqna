98868e9edae9e4d105200f6fd544630d06e3624e
David Padilla <david@crowdint.com>
2011-03-10 17:05:11 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrustTheCrowd::Application.config.secret_token = '7e632706f250c245c6bc5daf198293c86bffa3ded8c3304b5af2e88e0fde184cf35ccbd54d8708ab86c6e5c66abd976796c964e2e239384e75bbef07e37f0441'
