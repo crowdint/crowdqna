b88167b5c56dbc1ac093061c7ed14d88692d7141
David Padilla <david@crowdint.com>
2011-03-20 15:45:00 -0600

    is_admin?

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User
   #
   field :name, :type => String
 
+  #
+  # Allowed users
+  #
+  ADMIN_USERS = ['david.padilla@crowdint.com', 'esteban.cortes@crowdint.com']
+
+  def is_admin?
+    ADMIN_USERS.include?(self.email)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = Fabricate(:user)
+  end
+
+  describe "#is_admin?" do
+    context "The user email is in the whitelist" do
+      before :each do
+        @user.update_attributes(:email => User::ADMIN_USERS.first)
+      end
+
+      it "returns true" do
+        @user.is_admin?.should == true
+      end
+    end
+
+    context "The user is not on the whitelist" do
+      it "returns false" do
+        @user.is_admin?.should == false
+      end
+    end
+  end
 end
